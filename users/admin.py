ddbf66f1e4b9e50b5ee9223668a7e7503c6f5fb1
Earvin Gemenez <earvin.gemenez@gmail.com>
2021-01-18 11:30:38 +0800

    initial commit

---
+++ users/admin.py
@@ -0,0 +1,48 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from django.utils.translation import gettext_lazy as _
+from rest_framework.authtoken.models import Token
+
+
+class TokenAdmin(admin.TabularInline):
+    """ user auth token
+    """
+    model = Token
+    readonly_fields = ('key', 'created')
+
+
+@admin.register(get_user_model())
+class UserAdmin(BaseUserAdmin):
+    """ user configuration
+    """
+    readonly_fields = ('date_joined',)
+    ordering = ('email',)
+
+    filter_horizontal = ('groups', 'user_permissions')
+    list_display = ('email', 'first_name', 'last_name', 'date_joined')
+    inlines = (TokenAdmin,)
+
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'password')
+        }),
+        (_('Personal info'), {
+            'fields': ('first_name', 'last_name')
+        }),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser',
+                       'groups', 'user_permissions')
+        }),
+        (_('Important dates'), {
+            'fields': ('last_login', 'date_joined')
+        })
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
